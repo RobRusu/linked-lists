859b88e4aca3535aefaffadbb3c3ef8766e9d798
RobRusu <142660890+RobRusu@users.noreply.github.com>
2024-06-05 23:23:18 +0100

    classes for linked list and node created

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 
+class LinkedList {
+  constructor(value = null, nextNode = null) {
+    (this.value = value), (this.nextNode = nextNode);
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    (this.value = value), (this.nextNode = nextNode);
+  }
+}
