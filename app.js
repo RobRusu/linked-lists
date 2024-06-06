100d30ef6d6aeacd68cdc4ce5f83cd7505a90386
RobRusu <142660890+RobRusu@users.noreply.github.com>
2024-06-06 13:00:19 +0100

    append method added to the linked list

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
 class LinkedList {
-  constructor(value = null, nextNode = null) {
-    (this.value = value), (this.nextNode = nextNode);
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    if (this.head === null) {
+      //if head is empty add node
+      this.head = new Node(value);
+    } else {
+      //otherwise add value to the end of list
+      let head = this.head;
+      while (head.nextNode !== null) {
+        head = head.nextNode;
+      }
+      head.nextNode = new Node(value);
+    }
   }
 }
 
@@ -11,3 +25,10 @@ class Node {
     (this.value = value), (this.nextNode = nextNode);
   }
 }
+
+const list = new LinkedList();
+list.append(3);
+list.append(5);
+list.append(20);
+
+console.log(list.head);
